7dfd6b79bdeb39adcf217eb3a410c34fc2737275
aik2 <aik2mlj@gmail.com>
2023-11-01 17:20:15 +0400

    scripts

---
+++ Scripts/executable_patch-firefox-omni.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+patchfile=${1:-~/Scripts/firefox-omni.patch}
+echo $patchfile
+omni=/usr/lib/firefox/browser/omni.ja
+
+setopt -o err_exit
+setopt -o xtrace
+
+[[ -d /tmp/firefox-omni ]] && rm -rf /tmp/firefox-omni
+mkdir /tmp/firefox-omni
+cd /tmp/firefox-omni
+unzip -q $omni || true
+
+patch chrome/browser/content/browser/browser.xhtml $patchfile
+
+zip -0DXqr /tmp/omni.ja *
+
+sudo cp -v $omni $omni.orig
+sudo cp -v /tmp/omni.ja $omni
+
+rm -rf /tmp/omni.ja /tmp/firefox-omni
+rm -rf ~/.cache/mozilla/firefox/*/startupCache
