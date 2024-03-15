5d20b936c514ee879c6dbd027d45e0836122f107
aik2 <aik2mlj@gmail.com>
2024-03-15 15:01:10 +0800

    darkman: add btop-theme.sh

---
+++ dot_config/darkman/dark-mode.d/executable_btop-theme.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sed -i 's/color_theme.*/color_theme = "Default"/' $HOME/.config/btop/btop.conf
---
+++ dot_config/darkman/light-mode.d/executable_btop-theme.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sed -i 's/color_theme.*/color_theme = "\/usr\/share\/btop\/themes\/whiteout.theme"/' $HOME/.config/btop/btop.conf
