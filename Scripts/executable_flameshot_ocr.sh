e1653282a0b6cc77b0f5acc3c10ab4ecab966dcc
aik2 <aik2mlj@gmail.com>
2024-03-16 12:29:35 +0800

    spectacle/flameshot ocr script & add kde shortcut

--- Scripts/executable_flameshot_ocr.sh
+++
@@ -1,3 +0,0 @@
-flameshot gui --raw | tesseract -l eng+chi_sim stdin stdout | xclip -in -selection clipboard
-# for wayland
-# flameshot gui --raw | tesseract stdin stdout | wl-copy
---
+++ Scripts/executable_spectacle-ocr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Identify the session type
+session_type=$XDG_SESSION_TYPE
+tesseract_command="tesseract -l eng+chi_sim -c preserve_interword_spaces=1 stdin stdout"
+
+if [ "$session_type" == "wayland" ]; then
+    spectacle -b -n -o /proc/self/fd/1 | $tesseract_command | wl-copy
+else
+    spectacle -b -n -o /proc/self/fd/1 | $tesseract_command | xclip -in -selection clipboard
+fi
