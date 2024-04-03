ad60089b0e3ff6cc6f03cf611956348252245abe
aik2 <aik2mlj@gmail.com>
2024-04-03 21:57:37 +0800

    darkman: kitty auto reload
    plus some formatting

--- dot_config/darkman/light-mode.d/executable_kde-konsole-theme.sh
+++ dot_config/darkman/light-mode.d/executable_kde-konsole-theme.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# KDE's default terminal emulator supports profiles, you can create one in 
-# Settings > Manage Profiles. You can select a dark or light theme in 
+# KDE's default terminal emulator supports profiles, you can create one in
+# Settings > Manage Profiles. You can select a dark or light theme in
 # Appearance > Color scheme and font. The following script iterates over all
 # instances of Konsole und changes the profile of all sessions. This is necessary,
 # if there are multiple tabs in one of the Konsole instances.
@@ -9,16 +9,14 @@
 
 # get all running konsole instances
 KONSOLEPIDS=($(pidof konsole))
-for i in "${KONSOLEPIDS[@]}"
-    do
-        # get number of sessions this particular instance has
-        CURRENTSESSIONCOUNT=$(qdbus org.kde.konsole-$i /Windows/1 sessionCount)
-        for x in $(seq 1 $CURRENTSESSIONCOUNT)
-            do 
-                # change profile through dbus message
-                qdbus org.kde.konsole-$i /Sessions/$x setProfile "Light"
-            done
-    done
+for i in "${KONSOLEPIDS[@]}"; do
+	# get number of sessions this particular instance has
+	CURRENTSESSIONCOUNT=$(qdbus org.kde.konsole-$i /Windows/1 sessionCount)
+	for x in $(seq 1 $CURRENTSESSIONCOUNT); do
+		# change profile through dbus message
+		qdbus org.kde.konsole-$i /Sessions/$x setProfile "Light"
+	done
+done
 
 # change default profile
 sed -i 's/DefaultProfile.*/DefaultProfile=Light.profile/' $HOME/.config/konsolerc
