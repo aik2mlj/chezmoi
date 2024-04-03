562f392b2e04226cedb0db6f3a94e3ad5d877d68
aik2 <aik2mlj@gmail.com>
2024-04-03 21:57:37 +0800

    darkman: kitty auto reload
    plus some formatting

--- dot_config/darkman/dark-mode.d/executable_kitty-theme.sh
+++ dot_config/darkman/dark-mode.d/executable_kitty-theme.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 kitty +kitten themes kanagawabones
+
+# reload all kitty instances
+KITTYPIDS=($(pidof kitty))
+for i in "${KITTYPIDS[@]}"; do
+	kill -SIGUSR1 $i # see https://sw.kovidgoyal.net/kitty/conf/#kitty-conf
+done
--- dot_config/darkman/light-mode.d/executable_kitty-theme.sh
+++ dot_config/darkman/light-mode.d/executable_kitty-theme.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 kitty +kitten themes Gruvbox\ Light\ Hard
+
+# reload all kitty instances
+KITTYPIDS=($(pidof kitty))
+for i in "${KITTYPIDS[@]}"; do
+	kill -SIGUSR1 $i # see https://sw.kovidgoyal.net/kitty/conf/#kitty-conf
+done
