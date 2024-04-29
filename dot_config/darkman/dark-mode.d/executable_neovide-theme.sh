ece51b9c970186d7399dcd9dabd5673edd3c0ade
aik2 <aik2mlj@gmail.com>
2024-04-29 14:30:49 +0800

    astronvim v4: neovide-theme

--- dot_config/darkman/dark-mode.d/executable_neovide-theme.sh
+++ dot_config/darkman/dark-mode.d/executable_neovide-theme.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -i 's/neovide_theme.*/neovide_theme = "dark",/' $HOME/.config/nvim/lua/user/options.lua
+sed -i 's/neovide_theme.*/neovide_theme = "dark",/' $HOME/.config/nvim/lua/plugins/astrocore.lua
