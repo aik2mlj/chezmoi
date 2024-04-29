35c39087c7529d3a7a67be590fa75701e254a8b6
aik2 <aik2mlj@gmail.com>
2024-04-29 14:30:49 +0800

    astronvim v4: neovide-theme

--- dot_config/darkman/light-mode.d/executable_neovide-theme.sh
+++ dot_config/darkman/light-mode.d/executable_neovide-theme.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -i 's/neovide_theme.*/neovide_theme = "light",/' $HOME/.config/nvim/lua/user/options.lua
+sed -i 's/neovide_theme.*/neovide_theme = "light",/' $HOME/.config/nvim/lua/plugins/astrocore.lua
