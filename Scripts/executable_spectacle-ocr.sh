2459aca8aa746f6240346450cbf04a2a5f816e83
Lejun Min <aik2mlj@gmail.com>
2025-07-19 11:38:10 +0200

    fix spectacle-ocr.sh

--- Scripts/executable_spectacle-ocr.sh
+++ Scripts/executable_spectacle-ocr.sh
@@ -5,7 +5,7 @@ session_type=$XDG_SESSION_TYPE
 tesseract_command="tesseract -l eng+chi_sim -c preserve_interword_spaces=1 stdin stdout"
 
 if [ "$session_type" == "wayland" ]; then
-    spectacle -b -n -o /proc/self/fd/1 | $tesseract_command | wl-copy
+    spectacle -b -r -n -o /proc/self/fd/1 | $tesseract_command | wl-copy
 else
-    spectacle -b -n -o /proc/self/fd/1 | $tesseract_command | xclip -in -selection clipboard
+    spectacle -b -r -n -o /proc/self/fd/1 | $tesseract_command | xclip -in -selection clipboard
 fi
