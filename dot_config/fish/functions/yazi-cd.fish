83de7f5bc0b798183357b9796dae5337e6f3d0e7
Lejun Min <aik2mlj@gmail.com>
2025-11-07 22:09:30 -0800

    yazi-cd update

--- dot_config/fish/functions/yazi-cd.fish
+++ dot_config/fish/functions/yazi-cd.fish
@@ -1,8 +1,8 @@
 function yazi-cd
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        cd -- "$cwd"
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
     end
     command rm -f -- "$tmp"
 end
