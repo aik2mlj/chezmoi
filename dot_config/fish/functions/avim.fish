e9f15cfeb9e38b1431377a69271c5b4db82f81dc
Lejun Min <aik2mlj@gmail.com>
2026-02-26 02:40:56 -0800

    fish functions

---
+++ dot_config/fish/functions/avim.fish
@@ -0,0 +1,4 @@
+function avim --wraps='NVIM_APPNAME=astro nvim' --description 'alias avim=NVIM_APPNAME=astro nvim'
+  NVIM_APPNAME=astro nvim $argv
+        
+end
--- dot_config/fish/functions/sshmbz.fish
+++
@@ -1,3 +0,0 @@
-function sshmbz --wraps='sshpass -f ~/.ssh/mbz_ps ssh' --description 'alias sshmbz=sshpass -f ~/.ssh/mbz_ps ssh'
-  sshpass -f ~/.ssh/mbz_ps ssh $argv; 
-end
